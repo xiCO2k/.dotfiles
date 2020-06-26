366076e32bfc49f7deeb0592c817f105892948c8
Francisco Madeira <xico2k@gmail.com>
2020-06-26 20:42:25 +0100

    Add IP Alias

--- aliases.zsh
+++ aliases.zsh
@@ -16,6 +16,7 @@ alias chmox='chmod -x'
 alias v="vim"
 alias ungz="gunzip -k"
 alias hosts='subl /etc/hosts'
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 
 # Shortcuts
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
@@ -45,27 +46,7 @@ alias vrebuild="vagrant destroy --force && vagrant up"
 
 # Docker
 alias docker-composer="docker-compose"
-#alias dstop="docker stop $(docker ps -a -q)"
-#alias dpurgecontainers="dstop && docker rm $(docker ps -a -q)"
-#alias dpurgeimages="docker rmi $(docker images -q)"
-#dbuild() { docker build -t=$1 .; }
-#dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
 
 # Git
 alias g="git"
-alias gst="git status"
-alias gb="git branch"
-alias gc="git checkout"
-alias gl="git log --oneline --decorate --color"
-alias amend="git add . && git commit --amend --no-edit"
-alias commit="git add . && git commit -m"
-alias diff="git diff"
-alias force="git push --force"
-alias nuke="git clean -df && git reset --hard"
-alias pop="git stash pop"
-alias pull="git pull"
-alias push="git push"
-alias resolve="git add . && git commit --no-edit"
-alias stash="git stash -u"
-alias unstage="git restore --staged ."
 alias wip="commit wip"
