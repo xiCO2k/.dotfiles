2d0ba11ac44be019d98723f0e6877ef9b5d8ef29
Francisco Madeira <xico2k@gmail.com>
2020-07-11 16:14:39 +0100

    remove alias

--- aliases.zsh
+++ aliases.zsh
@@ -23,7 +23,6 @@ alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias reloadshell="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
-alias c="clear"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
