e966c4360ec2a1be3da5d71e68fd4c9a022d3a9e
Francisco Madeira <xico2k@gmail.com>
2021-10-18 01:10:52 +0100

    Add OP tinker function from @Philo01

--- functions.zsh
+++ functions.zsh
@@ -1,3 +1,12 @@
 function f() {
     find . -name "$1" 2>&1 | grep -v 'Permission denied' | grep -v 'Operation not permitted'
 }
+
+function tinker() {
+  if [ -z "$1" ]
+    then
+       php artisan tinker
+    else
+       php artisan tinker --execute="dd($1);"
+  fi
+}
