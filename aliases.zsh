de9585f8e913a26b7586c287142b78013683628e
Francisco Madeira <xico2k@gmail.com>
2021-11-02 19:44:18 +0000

    fix: Add `ps` aliases

--- aliases.zsh
+++ aliases.zsh
@@ -37,6 +37,7 @@ alias pf="pest --filter "
 alias pp="pest --parallel"
 alias pc="XDEBUG_MODE=coverage pest --coverage"
 alias ppc="XDEBUG_MODE=coverage pest --parallel --coverage"
+alias ps="phpstan analyse"
 
 # JS
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
